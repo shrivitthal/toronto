61f67cdddeb473448be9907bdeaa118d097b16d9
ali126191 <ali126191@gmail.com>
2019-03-29 09:45:52 -0600

    intial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  pathPrefix: `/mini-gatsbyv2-material-kit-react`,
+  pathPrefix: `/SVHM--TESTING`,
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-offline',
     'gatsby-plugin-sass',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
-        start_url: '/',
+        start_url: '/SVHM--TESTING/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-offline',
+
   ],
 }
