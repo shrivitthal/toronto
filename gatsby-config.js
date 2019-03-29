bdc5d98f42c4177733ef63f323d55fc19a81f356
ali126191 <ali126191@gmail.com>
2019-03-29 10:33:57 -0600

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/SVHM/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/img/favicon.png', // This path is relative to the root of the site.
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/SVHM/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
       },
     },
     'gatsby-plugin-offline',
